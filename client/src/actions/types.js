9681479af660d884a5549a93940cc548801d19e1
Tan Phan <tan.phan@alley51.com>
2019-04-22 01:37:22 +0700

    Add reducers, action types

--- client/src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const GET_TODO = 'GET_TODO';
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODOS = 'DELETE_TODOS';
-export const LOAD_TODOS = 'LOAD_TODOS';
-export const COMPLETE_TODO = 'COMPLETE_TODO';
--- client/src/reducers/todoReducer.js
+++ client/src/reducers/todoReducer.js
@@ -1,7 +1,48 @@
-const initialState = {};
+import {
+  GET_TODOS,
+  ADD_TODO,
+  DELETE_TODO,
+  COMPLETE_TODO,
+  LOAD_TODOS,
+} from '../actions/todoActionTypes';
+
+const initialState = {
+  loading: false,
+  todos: [],
+};
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_TODOS:
+      return {
+        ...state,
+        loading: false,
+        todos: action.payload,
+      };
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [action.payload, ...state.todos],
+      };
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo._id !== action.payload),
+      };
+    case COMPLETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          return todo._id === action.payload
+            ? { ...todo, completed: true }
+            : todo;
+        }),
+      };
+    case LOAD_TODOS:
+      return {
+        ...state,
+        loading: true,
+      };
     default:
       return state;
   }
