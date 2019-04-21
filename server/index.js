b2c70f970d1d6960ab1f0fcf013bdbcd33c2d85c
Tan Phan <tan.phan@alley51.com>
2019-04-21 16:47:35 +0700

    Add bodyParser

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const todoRoutes = require('./routes');
 
 mongoose.connect('mongodb://localhost/mern-todo', { useNewUrlParser: true });
+
 const app = express();
+
+// Config middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/', todoRoutes);
 
 app.listen(5555, () => console.log('Server is running on port 5555'));
