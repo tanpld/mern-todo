81f7e174adf4a2bdf9a915ae9722131c17b0bac0
Tan Phan <tan.phan@alley51.com>
2019-04-22 01:19:27 +0700

    Add redux

---
+++ client/src/actions/types.js
@@ -0,0 +1,5 @@
+export const GET_TODO = 'GET_TODO';
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODOS = 'DELETE_TODOS';
+export const LOAD_TODOS = 'LOAD_TODOS';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
